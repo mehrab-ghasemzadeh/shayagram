b41752e3595acb2b456ea5ec06ea9d8090085d01
mehrab-ghasemzadeh <mehrab.ghw@gmail.com>
2025-02-15 18:50:20 +0330

    sth

---
+++ src/stores/store.js
@@ -0,0 +1,16 @@
+
+import { defineStore } from 'pinia'
+
+const useCounterStore = defineStore('counter', {
+  state: () => ({
+    name: 'asb',
+  }),
+  getters: {
+
+  },
+  actions: {
+
+  },
+})
+
+export default useCounterStore;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <h1>adjaofn</h1>
+    <h2>{{ store.name }}</h2>
+  </main>
+</template>
+
+<script>
+  import useCounterStore from '../stores/store'
+  import { ref, computed } from 'vue'
+  export default{
+    setup(){
+      const counterStore = useCounterStore();
+      return {
+        store: computed(() => counterStore),
+      };
+    },
+  }
+</script>
